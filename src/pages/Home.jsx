74f8dab176de8def985c4707e2263d715771ff69
Shalmaliiii <79739505+Shalmaliiii@users.noreply.github.com>
2025-07-19 18:25:47 +0530

    feat: add routing and theme toggle functionality (#7)
    - Introduced React Router for navigation with Home and NotFound pages.
    - Implemented ThemeToggle component for dark/light mode switching.
    - Updated index.html to change the favicon and title.
    - Added a new SVG asset for the favicon.
    - Enhanced CSS with Tailwind configuration and custom utility classes.
    - Created Home and NotFound pages with basic structure.
    
    Co-authored-by: Sujai Dev P <58269892+SujaiDev@users.noreply.github.com>

---
+++ src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ThemeToggle from '../components/ThemeToggle'
+
+const Home = () => {
+  return (
+    <div className='min-h-screen bg-background text-foreground overflow-x-hidden'>
+      {/* Theme toggle */}
+        <ThemeToggle />
+      {/* Background effect */}
+
+      {/* Navbar  */}
+
+      {/* Content  */}
+
+      {/* Footer  */}
+
+    </div>
+
+  )
+}
+
+export default Home
